6496a2e1f795fbdb40eb737f07404ddfd40466c6
Connor Rigby <connorr@hey.com>
2021-01-27 14:50:09 -0800

    Enable CI

--- mix.exs
+++ mix.exs
@@ -1,13 +1,28 @@
 defmodule NervesHubLinkCommon.MixProject do
   use Mix.Project
 
+  @version "0.1.1"
+  @source_url "https://github.com/nerves-hub/nerves_hub_link_common"
+
   def project do
     [
       app: :nerves_hub_link_common,
-      version: "0.1.0",
-      elixir: "~> 1.10",
+      version: @version,
+      elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+        docs: :docs,
+        "hex.publish": :docs
+      ],
+      docs: docs(),
+      description: description(),
+      dialyzer: dialyzer(),
+      package: package(),
       deps: deps()
     ]
   end
@@ -21,8 +36,46 @@ defmodule NervesHubLinkCommon.MixProject do
       {:mint, "~> 1.2"},
       {:castore, "~> 0.1.0"},
       {:fwup, "~> 0.4.0"},
+      {:ex_doc, "~> 0.18", only: :docs, runtime: false},
+      {:excoveralls, "~> 0.10", only: :test},
       {:plug_cowboy, "~> 2.0", only: :test},
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp description do
+    "Common modules shared between nerves_hub_link and nerves_hub_link_http"
+  end
+
+  defp dialyzer() do
+    [
+      flags: [:race_conditions, :error_handling, :underspecs, :unmatched_returns],
+      plt_add_apps: [],
+      list_unused_filters: true
+    ]
+  end
+
+  defp docs do
+    [
+      extras: ["README.md", "CHANGELOG.md"],
+      main: "readme",
+      source_ref: "v#{@version}",
+      source_url: @source_url,
+      skip_undefined_reference_warnings_on: ["CHANGELOG.md"]
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => @source_url},
+      files: [
+        "lib",
+        "CHANGELOG.md",
+        "LICENSE",
+        "mix.exs",
+        "README.md"
+      ]
     ]
   end
 end
